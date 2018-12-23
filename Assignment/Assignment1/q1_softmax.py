8a3959cc65e606494bfe12a08fdf1b2cb0ff15cd
ooxx201 <ooxx.2b@gmail.com>
2018-12-23 07:38:01 -0800

    PS1 2%

--- Assignment/Assignment1/q1_softmax.py
+++ Assignment/Assignment1/q1_softmax.py
@@ -31,12 +31,18 @@ def softmax(x):
     if len(x.shape) > 1:
         # Matrix
         ### YOUR CODE HERE
-        raise NotImplementedError
+        x -= np.max(x, axis=1).reshape((-1, 1))
+        x = np.exp(x)
+        x /= np.sum(x, axis=1).reshape((-1, 1))
+        #raise NotImplementedError
         ### END YOUR CODE
     else:
         # Vector
         ### YOUR CODE HERE
-        raise NotImplementedError
+        x -= np.max(x)
+        x = np.exp(x)
+        x /= np.sum(x)
+        #raise NotImplementedError
         ### END YOUR CODE
 
     assert x.shape == orig_shape
@@ -84,4 +90,4 @@ def test_softmax():
 
 if __name__ == "__main__":
     test_softmax_basic()
-    test_softmax()
+    #test_softmax()
